a90ecb7479ef263c6133864032008248c389a98d
michael rondon <mrondon72@gmail.com>
2021-07-11 01:18:40 +0200

    63. Pagina - HomePage

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
