07d2eae94c51d119f2e83e9475587992e349c6af
michael rondon <mrondon72@gmail.com>
2021-07-11 02:29:00 +0200

    68. Navegar a una nueva pantalla en Flutter

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Pages
+import 'package:componentes/src/pages/alert_page.dart';
+
+// Providers
 import 'package:componentes/src/providers/menu_provider.dart';
+
+// Utils
 import 'package:componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,7 +24,7 @@ class HomePage extends StatelessWidget {
     return FutureBuilder(
       builder: ( BuildContext context, AsyncSnapshot<List<dynamic>> snapshot ) {
         return ListView(
-          children: _listaItems( snapshot.data )
+          children: _listaItems( snapshot.data, context )
         );
       },
       future: menuProvider.cargarData(),
@@ -26,13 +32,19 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  List<Widget> _listaItems( List<dynamic>? data ) {
+  List<Widget> _listaItems( List<dynamic>? data, BuildContext context ) {
     final List<Widget> opciones = [];
 
     data?.forEach( (opt) {
       final widgetTemp = ListTile(
         leading: getIcon( opt['icon'] ),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: ( context ) => AlertPage()
+          );
+
+          Navigator.push(context, route);
+        },
         title: Text( opt['texto'] ),
         trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue ),
       );
