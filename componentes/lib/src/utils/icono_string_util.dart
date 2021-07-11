05e2237d66ad11d22ac22b62ddb747f8e8df949c
michael rondon <mrondon72@gmail.com>
2021-07-12 01:02:22 +0200

    77. Container y AnimatedContainer

--- componentes/lib/src/utils/icono_string_util.dart
+++ componentes/lib/src/utils/icono_string_util.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 final _icons = <String, IconData> {
   'accessibility': Icons.accessibility,
   'add_alert': Icons.add_alert,
+  'donut_large': Icons.donut_large,
   'folder_open': Icons.folder_open
 };
 
