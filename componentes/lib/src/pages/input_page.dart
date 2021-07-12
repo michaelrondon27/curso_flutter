768391d5309996fa0f80901f6789c9a3dd6abb6b
michael rondon <mrondon72@gmail.com>
2021-07-12 15:07:40 +0200

    79. Email y Password fields

--- componentes/lib/src/pages/input_page.dart
+++ componentes/lib/src/pages/input_page.dart
@@ -6,6 +6,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String _email = '';
   String _nombre = '';
 
   @override
@@ -18,10 +19,32 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           _crearInput(),
           Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
           _crearPersona()
         ],
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
       ),
+    );
+  }
+
+  Widget _crearEmail() {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Email',
+        icon: Icon( Icons.email ),
+        labelText: 'Email',
+        suffixIcon: Icon( Icons.alternate_email )
+      ),
+      keyboardType: TextInputType.emailAddress,
+      onChanged: ( value ) => setState(() {
+        _email = value;
+      }),
     );
   }
 
@@ -39,18 +62,35 @@ class _InputPageState extends State<InputPage> {
         labelText: 'Nombre',
         suffixIcon: Icon( Icons.accessibility )
       ),
-      onChanged: ( value ) {
-        setState(() {
-          _nombre = value;
-        });
-      },
+      onChanged: ( value ) => setState(() {
+        _nombre = value;
+      }),
       textCapitalization: TextCapitalization.sentences,
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Password',
+        icon: Icon( Icons.lock ),
+        labelText: 'Password',
+        suffixIcon: Icon( Icons.lock_open )
+      ),
+      obscureText: true,
+      onChanged: ( value ) => setState(() {
+        _email = value;
+      }),
     );
   }
 
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
