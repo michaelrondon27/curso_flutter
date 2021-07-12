587d9ef43a1137ca302ee3a8b701532f8d1f6d6c
michael rondon <mrondon72@gmail.com>
2021-07-13 00:46:45 +0200

    99. Finalizar el MovieSlider

--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -11,6 +11,8 @@ class MovieSlider extends StatelessWidget {
             padding: EdgeInsets.symmetric( horizontal: 20 )
           ),
 
+          SizedBox( height: 5 ),
+
           Expanded(
             child: ListView.builder(
               itemBuilder: ( _, int index ) => _MoviePoster(),
@@ -21,8 +23,7 @@ class MovieSlider extends StatelessWidget {
         ],
         crossAxisAlignment: CrossAxisAlignment.start,
       ),
-      color: Colors.red,
-      height: 250,
+      height: 270,
       width: double.infinity,
     );
   }
@@ -32,9 +33,34 @@ class _MoviePoster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
+      child: Column(
+        children: [
+          GestureDetector(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                fit: BoxFit.cover,
+                height: 190,
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                placeholder: AssetImage('no-image.jpg'),
+                width: 130,
+              ),
+            ),
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-intance'),
+          ),
+
+          SizedBox( height: 5 ),
+
+          Text(
+            'Starwars: El retorno del nuevo Jedi',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
       height: 190,
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.symmetric( horizontal: 10 ),
       width: 130,
     );
   }
