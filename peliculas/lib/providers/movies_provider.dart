345a6299887fd96ba5735595ff524b4cba290e53
michael rondon <mrondon72@gmail.com>
2021-07-13 16:03:02 +0200

    105. Peticiones HTTP

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,13 +1,27 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
-  MoviesProvider() {
-    print('MoviesProvider inicializado');
+  String _apiUrl = '7a3f4cbe05801e3adbec1ac98275422d';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
 
+  MoviesProvider() {
     this.getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() {
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apiUrl,
+      'language': _language,
+      'page': '1'
+    });
+
+    final response = await http.get(url);
+    
+    final Map<String, dynamic>decodedData = json.decode( response.body );
+
+    
   }
 }
