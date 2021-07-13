8f14979198d8ad9050a4fca6b5465bf55c6e60b9
michael rondon <mrondon72@gmail.com>
2021-07-13 14:35:33 +0200

    101. Poster y titulo de la pelicula

--- peliculas/lib/screens/details_screen.dart
+++ peliculas/lib/screens/details_screen.dart
@@ -8,7 +8,13 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ]),
+          )
         ],
       )
     );
@@ -40,6 +46,67 @@ class _CustomAppBar extends StatelessWidget {
       ),
       floating: false,
       pinned: true,
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              height: 150,
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              placeholder: AssetImage('assets/no-image.jpg'),
+            ),
+          ),
+
+          SizedBox( width: 20 ),
+
+          Column(
+            children: [
+              Text(
+                'movie.title',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: textTheme.headline5
+              ),
+
+              Text(
+                'movie.originalTitle',
+                overflow: TextOverflow.ellipsis,
+                style: textTheme.subtitle1
+              ),
+
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    color: Colors.grey,
+                    size: 15,                    
+                  ),
+
+                  SizedBox( width: 5 ),
+
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          )
+        ],
+      ),
+      margin: EdgeInsets.only( top: 20),
+      padding: EdgeInsets.symmetric( horizontal: 20 ),
     );
   }
 }
