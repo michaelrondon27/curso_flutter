199d13a0713a82c2633288718d9a8f8b79ad80dd
michael rondon <mrondon72@gmail.com>
2021-07-15 22:40:25 +0200

    131. Diseño basico - parte 2

--- disenos/lib/screens/basic_design.dart
+++ disenos/lib/screens/basic_design.dart
@@ -4,11 +4,52 @@ class BasicDesingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/landscape.jpg'),
-        )
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage('assets/landscape.jpg'),
+          ),
+
+          Title()
+        ],
       ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                )
+              ),
+
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.black45
+                )
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+
+          Expanded(child: Container()),
+
+          Icon( Icons.star, color: Colors.red ),
+
+          Text('41')
+        ],
+      ),
+      margin: EdgeInsets.symmetric( horizontal: 30, vertical: 10 ),
     );
   }
 }
