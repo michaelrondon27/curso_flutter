02ed833b6616e0c02318c7660b7d0fdc6282cf2b
michael rondon <mrondon72@gmail.com>
2021-07-15 22:51:43 +0200

    132. Diseño basico - parte 3

--- disenos/lib/screens/basic_design.dart
+++ disenos/lib/screens/basic_design.dart
@@ -10,9 +10,56 @@ class BasicDesingScreen extends StatelessWidget {
             image: AssetImage('assets/landscape.jpg'),
           ),
 
-          Title()
+          Title(),
+
+          ButtonSection(),
+
+          Container(
+            child: Text('Aliquip magna do nisi irure sunt ipsum culpa duis. Nulla culpa aliquip ipsum consectetur occaecat enim ad aliqua aliqua pariatur ullamco nulla occaecat. Quis velit Lorem nostrud occaecat deserunt. Ad minim aliqua ea irure quis cillum.'),
+            margin: EdgeInsets.symmetric( horizontal: 20, vertical: 10 ),
+          )
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          CustomButton( icon: Icons.call, text: 'Call' ),
+
+          CustomButton( icon: Icons.map_sharp, text: 'Route' ),
+          
+          CustomButton( icon: Icons.share, text: 'Share' )
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      ),
+      margin: EdgeInsets.symmetric( horizontal: 20, vertical: 10 ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key, 
+    required this.icon, 
+    required this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue, size: 30),
+        Text(this.text, style: TextStyle(color: Colors.blue))
+      ],
     );
   }
 }
