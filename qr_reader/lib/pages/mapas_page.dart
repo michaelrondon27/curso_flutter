757724e397e8d72daea26a5e5d37c6f9ad22b1aa
michael rondon <mrondon72@gmail.com>
2021-07-19 00:44:48 +0200

    165. Mostrar Scans UI - Sin base de datos

--- qr_reader/lib/pages/mapas_page.dart
+++ qr_reader/lib/pages/mapas_page.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mapas Page')
+    return ListView.builder(
+      itemBuilder: ( _, i ) => ListTile(
+        leading: Icon( Icons.map, color: Theme.of(context).primaryColor ),
+        onTap: () => print('abrir algo'),
+        subtitle: Text('ID: $i'),
+        title: Text('Prueba $i'),
+        trailing: Icon( Icons.keyboard_arrow_right, color: Colors.grey )
+      ),
+      itemCount: 10,
     );
   }
 }
