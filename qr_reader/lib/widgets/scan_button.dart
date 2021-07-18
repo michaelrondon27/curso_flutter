1b14b0e854160f1bddd6f9c69c3783c3975b139a
michael rondon <mrondon72@gmail.com>
2021-07-18 23:19:13 +0200

    156. Modelo para manejar los Scans

--- qr_reader/lib/widgets/scan_button.dart
+++ qr_reader/lib/widgets/scan_button.dart
@@ -8,12 +8,14 @@ class ScanButton extends StatelessWidget {
       child: Icon( Icons.filter_center_focus ),
       elevation: 0,
       onPressed: () async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#3D8BEF', 
-          'Cancelar', 
-          false, 
-          ScanMode.QR
-        );
+        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        //   '#3D8BEF', 
+        //   'Cancelar', 
+        //   false, 
+        //   ScanMode.QR
+        // );
+
+        final barcodeScanRes = 'https://www.instagram.com/mikerondon27/';
 
         print(barcodeScanRes);
       },
