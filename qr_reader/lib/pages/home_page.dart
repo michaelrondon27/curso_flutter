aed2af3bbedf623c35aa5216cde6aa1248ff7455
michael rondon <mrondon72@gmail.com>
2021-07-18 21:26:50 +0200

    148. Diseño de la aplicacion

--- qr_reader/lib/pages/home_page.dart
+++ qr_reader/lib/pages/home_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon( Icons.delete_forever ),
+            onPressed: () {},
+          )
+        ],
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Historial'),
+      ),
       body: Center(
         child: Text('Home Page')
       ),
+      bottomNavigationBar: CustomNAvigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
