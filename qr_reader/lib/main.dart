f3af92270fc179f922fec2346d70099391e80c0a
michael rondon <mrondon72@gmail.com>
2021-07-18 21:16:44 +0200

    147. Inicio de proyecto - QRScanner

---
+++ qr_reader/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:qr_reader/pages/home_page.dart';
+import 'package:qr_reader/pages/mapa_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => HomePage(),
+        'mapa': ( _ ) => MapaPage()
+      },
+      title: 'QR Reader',
+    );
+  }
+}
