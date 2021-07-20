e1017974c64e96df4e6f07cea7f921bf917544d6
michael rondon <mrondon72@gmail.com>
2021-07-20 22:04:26 +0200

    183. Inicio del proyecto - Prefenrencias de Usuario

---
+++ preferenciasusuarioapp/lib/src/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Preferencias de Usuario'),
+      ),
+      body: Column(
+        children: [
+          Text('Color secundario:'),
+
+          Divider(),
+
+          Text('Género:'),
+
+          Divider(),
+          
+          Text('Nombre de usuario:'),
+
+          Divider(),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    );
+  }
+}
