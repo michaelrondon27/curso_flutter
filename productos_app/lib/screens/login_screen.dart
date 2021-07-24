2bff8a52d234a376e3f0cddaac83145c0e68136a
michael rondon <mrondon72@gmail.com>
2021-07-25 00:02:33 +0200

    197. Formulario de Login

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -23,11 +23,57 @@ class LoginScreen extends StatelessWidget {
                     ),
 
                     SizedBox( height: 30 ),
+
+                    _LoginForm()
                   ],
                 )
+              ),
+
+              SizedBox( height: 50 ),
+            
+              Text(
+                'Crear una nueva cuenta',
+                style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold ),
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                hintText: 'john.doe@gmail.com',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                labelText: 'Correo electrónico',
+                prefixIcon: Icon( Icons.alternate_email_sharp, color: Colors.deepPurple )
+              ),
+              keyboardType: TextInputType.emailAddress,
+            )
+          ],
         ),
       ),
     );
