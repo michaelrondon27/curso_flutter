2563a35b40376916a7c7eb51fda2ec85c4eb9aed
michael rondon <mrondon72@gmail.com>
2021-07-25 15:00:57 +0200

    208. Widget - ProductCard

--- productos_app/lib/widgets/product_card.dart
+++ productos_app/lib/widgets/product_card.dart
@@ -5,12 +5,61 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       child: Container(
-        color: Colors.red,
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+
+            _ProductDetails()
+          ],
+        ),
+        decoration: _cardBorders(),
         margin: EdgeInsets.only( bottom: 50, top: 30 ),
         height: 400,
         width: double.infinity,
       ),
       padding: const EdgeInsets.symmetric( horizontal: 20 ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+    borderRadius: BorderRadius.circular(25),
+    boxShadow: [
+      BoxShadow(
+        blurRadius: 10,
+        color: Colors.black12,
+        offset: Offset(0, 7)
+      )
+    ],
+    color: Colors.white,
+  );
+}
+
+class _BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular( 25 ),
+      child: Container(
+        child: FadeInImage(
+          fit: BoxFit.cover,
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        ),
+        height: 400,
+        width: double.infinity,
+      ),
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 70,
+      width: double.infinity
     );
   }
 }
