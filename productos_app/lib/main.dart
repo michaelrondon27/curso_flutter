419451e86e6ab86270ac1224df2357aaecfc4583
michael rondon <mrondon72@gmail.com>
2021-07-26 20:41:09 +0200

    234. Inicio de proyecto - Login y manejo de tokens

--- productos_app/lib/main.dart
+++ productos_app/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'home',
+      initialRoute: 'login',
       routes: {
         'home' : ( _ ) => HomeScreen(),
         'login': ( _ ) => LoginScreen(),
