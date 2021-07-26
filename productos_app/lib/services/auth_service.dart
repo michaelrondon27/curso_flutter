91f3351c81ea676000af56d72401dc8a4981a20e
michael rondon <mrondon72@gmail.com>
2021-07-26 22:26:52 +0200

    245. Usar token para validar peticiones en Firebase

--- productos_app/lib/services/auth_service.dart
+++ productos_app/lib/services/auth_service.dart
@@ -14,7 +14,8 @@ class AuthService extends ChangeNotifier {
   Future<String?> createUser( String email, String password ) async {
     final Map<String, dynamic> authData = {
       'email': email,
-      'password': password
+      'password': password,
+      'returnSecureToken': true
     };
 
     final url = Uri.https( _baseUrl , '/v1/accounts:signUp', {
@@ -37,7 +38,8 @@ class AuthService extends ChangeNotifier {
   Future<String?> login( String email, String password ) async {
     final Map<String, dynamic> authData = {
       'email': email,
-      'password': password
+      'password': password,
+      'returnSecureToken': true
     };
 
     final url = Uri.https( _baseUrl , '/v1/accounts:signInWithPassword', {
--- productos_app/lib/services/products_service.dart
+++ productos_app/lib/services/products_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:productos_app/models/models.dart';
@@ -12,6 +13,8 @@ class ProductsService extends ChangeNotifier {
   final List<Product> products = [];
 
   late Product selectedProduct;
+
+  final storage = new FlutterSecureStorage();
 
   File? newPictureFile;
 
@@ -26,7 +29,9 @@ class ProductsService extends ChangeNotifier {
     this.isLoading = true;
     notifyListeners();
 
-    final url = Uri.https( _baseUrl, 'products.json');
+    final url = Uri.https( _baseUrl, 'products.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
 
     final resp = await http.get( url );
 
@@ -61,7 +66,9 @@ class ProductsService extends ChangeNotifier {
   }
 
   Future<String> updateProduct( Product product ) async {
-    final url = Uri.https( _baseUrl, 'products/${ product.id }.json');
+    final url = Uri.https( _baseUrl, 'products/${ product.id }.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
 
     final resp = await http.put( url, body: product.toJson() );
 
@@ -75,7 +82,9 @@ class ProductsService extends ChangeNotifier {
   }
 
   Future<String> createProduct( Product product ) async {
-    final url = Uri.https( _baseUrl, 'products.json');
+    final url = Uri.https( _baseUrl, 'products.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
 
     final resp = await http.post( url, body: product.toJson() );
 
