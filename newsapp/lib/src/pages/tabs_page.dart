27bc6d9c013e1047f8d4cae2c347b713e3a53f9f
michael rondon <mrondon72@gmail.com>
2021-07-28 00:56:36 +0200

    277. Implementando el BottomNavigationBar

--- newsapp/lib/src/pages/tabs_page.dart
+++ newsapp/lib/src/pages/tabs_page.dart
@@ -4,18 +4,46 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            color: Colors.red,
-          ),
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
 
-          Container(
-            color: Colors.green,
-          )
-        ],
-        physics: BouncingScrollPhysics(),
-      ),
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon( Icons.person_outline ),
+          label: 'Para ti' 
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon( Icons.public ),
+          label: 'Encabezados' 
+        ),
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+
+        Container(
+          color: Colors.green,
+        )
+      ],
+      physics: NeverScrollableScrollPhysics(),
     );
   }
 }
--- newsapp/lib/src/theme/tema.dart
+++ newsapp/lib/src/theme/tema.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 final miTema = ThemeData.dark().copyWith(
-  accentColor: Colors.red
+  accentColor: Colors.red,
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.red,
+  )
 );
