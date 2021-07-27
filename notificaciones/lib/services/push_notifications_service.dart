fdb16b0a8fb50b04e26b3eb8744b5c4ea346ef0a
michael rondon <mrondon72@gmail.com>
2021-07-28 00:08:27 +0200

    266. Escuchar notificaciones - onBackground, onMessage, onOpenedApp

--- notificaciones/lib/services/push_notifications_service.dart
+++ notificaciones/lib/services/push_notifications_service.dart
@@ -8,9 +8,29 @@ class PushNotificationsService {
 
   static String? token;
 
+  static Future _backgroundHandler( RemoteMessage message ) async {
+    print('onBackground Handler ${ message.messageId }');
+  }
+
+  static Future _onMessageHandler( RemoteMessage message ) async {
+    print('onMessage Handler ${ message.messageId }'); 
+  }
+
+  static Future _onMessageOpenApp( RemoteMessage message ) async {
+    print('onMessageOpenApp Handler ${ message.messageId }'); 
+  }
+
   static Future initializeApp() async {
     await Firebase.initializeApp();
 
     token = await FirebaseMessaging.instance.getToken();
+
+    print('token: $token');
+
+    FirebaseMessaging.onBackgroundMessage( _backgroundHandler );
+
+    FirebaseMessaging.onMessage.listen( _onMessageHandler );
+
+    FirebaseMessaging.onMessageOpenedApp.listen( _onMessageOpenApp );
   }
 }
