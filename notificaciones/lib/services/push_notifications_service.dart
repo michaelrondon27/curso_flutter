6e91ecbdae58794e79120a27e8708001eec7bbbc
michael rondon <mrondon72@gmail.com>
2021-07-27 23:54:56 +0200

    265. Obtener el token del dispositivo

--- notificaciones/lib/services/push_notifications_service.dart
+++ notificaciones/lib/services/push_notifications_service.dart
@@ -1,1 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 // SHA1: 68:5B:8F:63:90:A0:CA:67:71:48:B9:6C:7D:19:2D:C0:2C:06:71:34
+
+class PushNotificationsService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  static String? token;
+
+  static Future initializeApp() async {
+    await Firebase.initializeApp();
+
+    token = await FirebaseMessaging.instance.getToken();
+  }
+}
