16c94f07433f34074a73783d0b25656a9e286519
michael rondon <mrondon72@gmail.com>
2021-07-29 18:41:16 +0200

    287. Loading de los Headlines y KeppAlive

--- newsapp/lib/src/pages/tabs_page.dart
+++ newsapp/lib/src/pages/tabs_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:newsapp/src/pages/tab1_page.dart';
+import 'package:newsapp/src/pages/tab2_page.dart';
 
 class TabsPage extends StatelessWidget {
   @override
@@ -48,9 +49,7 @@ class _Paginas extends StatelessWidget {
       children: [
         Tab1Page(),
 
-        Container(
-          color: Colors.green,
-        )
+        Tab2Page()
       ],
       controller: navegacionModel.pageController,
       physics: NeverScrollableScrollPhysics(),
