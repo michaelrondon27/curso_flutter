0123666f10494a7a09eabb92bb3a7f4783f132ee
michael rondon <mrondon72@gmail.com>
2021-07-29 18:09:27 +0200

    284. Diseño del widget de las noticias

--- newsapp/lib/src/widgets/lista_noticias.dart
+++ newsapp/lib/src/widgets/lista_noticias.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:newsapp/src/models/news_models.dart';
+import 'package:newsapp/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
   final List<Article> noticias;
@@ -11,9 +12,87 @@ class ListaNoticias extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return Text( this.noticias[index].title );
+        return _Noticia( noticia: this.noticias[index], index: index );
       },
       itemCount: this.noticias.length,
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({
+    required this.noticia,
+    required this.index
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TarjetaTopBar( noticia, index ),
+
+        _TarjetaTitulo( noticia ),
+
+        _TarjetaImagen( noticia )
+      ],
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaImagen( this.noticia );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo( this.noticia );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        noticia.title,
+        style: TextStyle( fontSize: 20, fontWeight: FontWeight.w700 )
+      ),
+      padding: EdgeInsets.symmetric( horizontal: 15 )
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+  
+  const _TarjetaTopBar( this.noticia, this.index );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text(
+            '${ index + 1 }. ',
+            style: TextStyle( color: miTema.accentColor )
+          ),
+
+          Text('${ noticia.source.name }. ')
+        ]
+      ),
+      margin: EdgeInsets.only( bottom: 10 ),
+      padding: EdgeInsets.symmetric( horizontal: 10 )
     );
   }
 }
