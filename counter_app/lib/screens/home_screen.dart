7d3c03e8533810d41e2a2dd13a7345236f0dd037
Michael Rondon <mrondon72@gmail.com>
2023-01-25 17:45:00 +0100

    51. Boton flotante - FloatingActionButton

--- counter_app/lib/screens/home_screen.dart
+++ counter_app/lib/screens/home_screen.dart
@@ -21,7 +21,11 @@ class HomeScreen extends StatelessWidget {
             Text("10", style: fontSize)
           ]
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {}
+      ),
     );
   }
 
