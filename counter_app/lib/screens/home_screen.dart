612d5497eb96902a243c7dbd582a17aa6b248385
Michael Rondon <mrondon72@gmail.com>
2023-01-25 16:44:47 +0100

    47. BuilContext y Scaffold

--- counter_app/lib/screens/home_screen.dart
+++ counter_app/lib/screens/home_screen.dart
@@ -6,8 +6,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("HomeScreen")
+    return const Scaffold(
+      body: Center(
+        child: Text("HomeScreen")
+      )
     );
   }
 
