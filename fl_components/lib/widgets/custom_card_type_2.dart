df407c0c7a02e4bb9935f6d3484c26cb480b0518
Michael Rondon <mrondon72@gmail.com>
2023-01-26 12:13:38 +0100

    79. Ajustar el tamaña de la imagen

--- fl_components/lib/widgets/custom_card_type_2.dart
+++ fl_components/lib/widgets/custom_card_type_2.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/theme/app_theme.dart';
+
 class CustomCardType2 extends StatelessWidget {
    
   const CustomCardType2({Key? key}) : super(key: key);
@@ -7,12 +9,27 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      elevation: 30,
+      shadowColor: AppTheme.primaryColor.withOpacity(0.5),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18)
+      ),
       child: Column(
         children: [
-          FadeInImage(
+          const FadeInImage(
+            fadeInDuration: Duration(milliseconds: 300),
+            fit: BoxFit.fill,
+            height: 230,
             image: NetworkImage('https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20new%20zealand%20S-shape.jpg'),
-            placeholder: AssetImage('assets/jar-loading.gif')
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+          ),
+
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(bottom: 10, right: 20, top: 10),
+            child: const Text('Un hermoso paisaje')
           )
         ],
       ),
