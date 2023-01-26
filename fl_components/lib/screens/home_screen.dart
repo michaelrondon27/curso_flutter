08b91fe46c7f3d427253a7629543def76dbafd82
Michael Rondon <mrondon72@gmail.com>
2023-01-26 10:31:43 +0100

    69. Diseño del HomeScreen

--- fl_components/lib/screens/home_screen.dart
+++ fl_components/lib/screens/home_screen.dart
@@ -6,10 +6,22 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Componentes en Flutter')
       ),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, i) => ListTile(
+          leading: const Icon(Icons.access_time_outlined),
+          title: const Text('Nombre de ruta'),
+          onTap: () {
+            
+          }
+        ), 
+        separatorBuilder: (_, __) => const Divider() 
+      )
     );
   }
 }
