1381f4194146ae05be202320055cb332a9d192fb
Michael Rondon <mrondon72@gmail.com>
2023-01-27 17:42:17 +0100

    Peticiones HTTP

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,13 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+
+  final String _apiKey = '7a3f4cbe05801e3adbec1ac98275422d';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
 
   MoviesProvider() {
     getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1'
+    });
 
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
   }
 
 }
