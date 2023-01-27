d5ffcd6776e4d18fd02c7818f03f6c5f3262cbbe
Michael Rondon <mrondon72@gmail.com>
2023-01-27 15:30:07 +0100

    109. Swiper - Carrusel de tarjetas

--- peliculas/lib/widgets/card_swiper.dart
+++ peliculas/lib/widgets/card_swiper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class CardSwiper extends StatelessWidget {
    
@@ -6,10 +7,29 @@ class CardSwiper extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Container(
-      color: Colors.red,
-      height: 500,
-      width: double.infinity
+      height: size.height * 0.5,
+      width: double.infinity,
+      child: Swiper(
+        itemBuilder: (_, i) => GestureDetector(
+          onTap: () => Navigator.pushNamed(context, '/details'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              fit: BoxFit.cover,
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+              placeholder: AssetImage('assets/no-image.jpg'), 
+            ),
+          )
+        ),
+        itemCount: 10,
+        itemHeight: size.height * 0.4,
+        itemWidth: size.width * 0.6,
+        layout: SwiperLayout.STACK,
+      )
     );
   }
 
