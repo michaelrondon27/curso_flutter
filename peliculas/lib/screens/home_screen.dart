6cedaa353093ab5c1036829758181843cb8c224e
Michael Rondon <mrondon72@gmail.com>
2023-01-28 16:04:57 +0100

    128. Implementar un SearchDelegate

--- peliculas/lib/screens/home_screen.dart
+++ peliculas/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
 import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,7 +19,7 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.search_outlined),
-            onPressed: () {}, 
+            onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate()), 
           )
         ],
         title: const Text('Películas en cines')
