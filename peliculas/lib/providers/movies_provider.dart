b5b1c78d349b750c1ffb4432470372e479d06c3e
Michael Rondon <mrondon72@gmail.com>
2023-01-28 14:53:47 +0100

    122. Optimizacion de codigo

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -13,34 +13,37 @@ class MoviesProvider extends ChangeNotifier {
   List<MovieModel> onDisplayMovies = [];
   List<MovieModel> popularMovies = [];
 
+  int _popularPage = 0;
+
   MoviesProvider() {
     getOnDisplayMovies();
     getPopularMovies();
   }
 
-  getOnDisplayMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+  Future<String> _getJsonData(String endpoint, {int page = 1}) async {
+    var url = Uri.https(_baseUrl, endpoint, {
       'api_key': _apiKey,
       'language': _language,
-      'page': '1'
+      'page': '$page'
     });
 
     final response = await http.get(url);
-    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    return response.body;
+  }
+
+  getOnDisplayMovies() async {
+    final jsonData = await _getJsonData('3/movie/now_playing');
+    final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
     onDisplayMovies = nowPlayingResponse.results;
 
     notifyListeners();
   }
 
   getPopularMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/popular', {
-      'api_key': _apiKey,
-      'language': _language,
-      'page': '1'
-    });
+    _popularPage++;
 
-    final response = await http.get(url);
-    final popularResponse = PopularResponse.fromJson(response.body);
+    final jsonData = await _getJsonData('3/movie/popular');
+    final popularResponse = PopularResponse.fromJson(jsonData);
     popularMovies = [...popularMovies, ...popularResponse.results];
 
     notifyListeners();
