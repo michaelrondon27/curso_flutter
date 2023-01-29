0a0a2c852eb83edf1bdb8a3322994e161b0497f3
Michael Rondon <mrondon72@gmail.com>
2023-01-29 15:00:43 +0100

    170. SQFLite - CREATE Table y Database

--- qr_reader/lib/providers/db_provider.dart
+++ qr_reader/lib/providers/db_provider.dart
@@ -1,22 +1,42 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
 
-  static late Database _database;
+  static Database? _database;
   static final DBProvider db = DBProvider._();
 
   DBProvider._();
 
   Future<Database> get databse async {
-    if (_database != null ) return _database;
+    if (_database != null ) return _database!;
 
     _database = await initDB();
 
-    return _database;
+    return _database!;
   }
 
   Future<Database> initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
 
+    return await openDatabase(
+      path,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE Scans(
+            id INTEGER PRIMARY KEY,
+            tipo TEXT,
+            valor TEXT
+          )
+        ''');
+      },
+      onOpen: (db) { },
+      version: 1
+    );
   }
 
 }
