21431d52c141070fb40df78c18c14db95eb62fd7
Michael Rondon <mrondon72@gmail.com>
2023-01-29 20:41:32 +0100

    208. Formmulario de Login

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -22,12 +22,56 @@ class LoginScreen extends StatelessWidget {
 
                     Text('Login', style: Theme.of(context).textTheme.headlineMedium),
 
-                    const SizedBox(height: 30)
+                    const SizedBox(height: 30),
+
+                    _LoginForm()
                   ]
+                )
+              ),
+
+              const SizedBox(height: 50),
+
+              const Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
                 )
               )
             ]
           )
+        )
+      )
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple)),
+                focusedBorder: UnderlineInputBorder(borderSide: BorderSide(
+                  color: Colors.deepPurple,
+                  width: 2
+                )),
+                hintText: 'john.doe@gmail.com',
+                labelStyle: TextStyle(color: Colors.grey),
+                labelText: 'Correo Electrónico',
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple)
+              ),
+              keyboardType: TextInputType.emailAddress
+            )
+          ]
         )
       )
     );
