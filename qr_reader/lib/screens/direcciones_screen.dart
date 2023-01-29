e314e6d1e213794a308c954c6ba3f5faa633052d
Michael Rondon <mrondon72@gmail.com>
2023-01-29 16:07:48 +0100

    181. Optimizar la pantalla de direcciones y mapas

--- qr_reader/lib/screens/direcciones_screen.dart
+++ qr_reader/lib/screens/direcciones_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/widgets/scan_tiles.dart';
 
 class DireccionesScreen extends StatelessWidget {
    
@@ -9,25 +8,8 @@ class DireccionesScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    
-    final scans = Provider.of<ScanListProvider>(context).scans;
-
-    return ListView.builder(
-      itemBuilder: (_, i) => Dismissible(
-        background: Container(
-          color: Colors.red,
-        ),
-        key: UniqueKey(),
-        onDismissed: (DismissDirection direction) => Provider.of<ScanListProvider>(context, listen: false).borrarScanPorId(scans[i].id!),
-        child: ListTile(
-          leading: Icon(Icons.home_outlined, color: Theme.of(context).primaryColor),
-          onTap: () {},
-          subtitle: Text(scans[i].id.toString()),
-          title: Text(scans[i].valor),
-          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey)
-        ),
-      ),
-      itemCount: scans.length
+    return const ScanTiles(
+      tipo: 'http'
     );
   }
 }
---
+++ qr_reader/lib/widgets/scan_tiles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/scan_list_provider.dart';
+
+class ScanTiles extends StatelessWidget {
+
+  final String tipo;
+   
+  const ScanTiles({
+    Key? key,
+    required this.tipo
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    final scans = Provider.of<ScanListProvider>(context).scans;
+
+    return ListView.builder(
+      itemBuilder: (_, i) => Dismissible(
+        background: Container(
+          color: Colors.red,
+        ),
+        key: UniqueKey(),
+        onDismissed: (DismissDirection direction) => Provider.of<ScanListProvider>(context, listen: false).borrarScanPorId(scans[i].id!),
+        child: ListTile(
+          leading: Icon(
+            tipo == 'htpp' ? Icons.map : Icons.home_outlined,
+            color: Theme.of(context).primaryColor
+          ),
+          onTap: () {},
+          subtitle: Text(scans[i].id.toString()),
+          title: Text(scans[i].valor),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey)
+        ),
+      ),
+      itemCount: scans.length
+    );
+  }
+}
