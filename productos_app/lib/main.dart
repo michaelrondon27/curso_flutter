bc55649ad1d8dd42d89e92c9f0cdd8072677eb9b
Michael Rondon <mrondon72@gmail.com>
2023-01-29 19:14:29 +0100

    204. Inicio de proyecto - ProductosApp

---
+++ productos_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/home': (_) => const HomeScreen(),
+        '/login': (_) => const LoginScreen()
+      },
+      title: 'Productos App'
+    );
+  }
+
+}
