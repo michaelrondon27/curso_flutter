041807a9d02b4dcc2dfb8d03eccbf351a1cb66bc
Michael Rondon <mrondon72@gmail.com>
2023-01-29 16:50:41 +0100

    185. Mostrar GoogleMap

--- qr_reader/lib/screens/mapa_screen.dart
+++ qr_reader/lib/screens/mapa_screen.dart
@@ -1,11 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:qr_reader/models/scan_model.dart';
 
-class MapaScreen extends StatelessWidget {
+class MapaScreen extends StatefulWidget {
    
   const MapaScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapaScreen> createState() => _MapaScreenState();
+
+}
+
+class _MapaScreenState extends State<MapaScreen> {
+
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
+  static const CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -15,9 +34,14 @@ class MapaScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mapa')
       ),
-      body: const Center(
-         child: Text('MapaScreen'),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
+
 }
