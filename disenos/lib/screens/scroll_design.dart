5fdd6c033492e33a79c17ba9567a17b042acbd0c
Michael Rondon <mrondon72@gmail.com>
2023-01-29 03:10:20 +0100

    147. Diseño con scroll - parte 3

--- disenos/lib/screens/scroll_design.dart
+++ disenos/lib/screens/scroll_design.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 
 class ScrollDesignScreen extends StatelessWidget {
+
+  final boxDecoration = const BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      colors: [
+        Color(0xFF5EE8C5),
+        Color(0xFF30BAD6)
+      ],
+      end: Alignment.bottomCenter,
+      stops: [0.5, 0.5]
+    )
+  );
    
   const ScrollDesignScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: const [
-          Background(),
-
-          MainContent()
-        ]
+      body: Container(
+        decoration: boxDecoration,
+        child: PageView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: const [
+            Page1(),
+            Page2()
+          ]
+        ),
       )
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        Background(),
+
+        MainContent()
+      ]
     );
   }
 }
@@ -66,6 +98,38 @@ class MainContent extends StatelessWidget {
           const Icon(Icons.keyboard_arrow_down, size: 100, color: Colors.white)
         ]
       ),
+    );
+  }
+
+}
+
+class Page2 extends StatelessWidget {
+  
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF30BAD6),
+      child: Center(
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor: const Color(0xFF0098FA),
+            shape: const StadiumBorder()
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30
+              )
+            ),
+          )
+        )
+      )
     );
   }
 
