303e974cf074a04a6856dc497aacf0dc2dfe2f12
Michael Rondon <mrondon72@gmail.com>
2023-01-29 16:56:10 +0100

    186. Cambiar tipo de mapa y punto central

--- qr_reader/lib/screens/mapa_screen.dart
+++ qr_reader/lib/screens/mapa_screen.dart
@@ -18,10 +18,6 @@ class _MapaScreenState extends State<MapaScreen> {
 
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
-  static const CameraPosition puntoInicial = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
 
 
 
@@ -30,12 +26,17 @@ class _MapaScreenState extends State<MapaScreen> {
 
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
     
+    final CameraPosition puntoInicial = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 17
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mapa')
       ),
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
