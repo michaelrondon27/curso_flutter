f4bf5167211ffe62d1794a73c1a94399f1b47dd7
Michael Rondon <mrondon72@gmail.com>
2023-01-29 13:47:01 +0100

    160. Diseño de la aplicacion

--- qr_reader/lib/screens/home_screen.dart
+++ qr_reader/lib/screens/home_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete_forever),
+            onPressed: () {},
+          )
+        ],
+        elevation: 0,
+        title: const Text('Historial')
+      ),
+      body: const Center(
          child: Text('HomeScreen'),
       ),
+      bottomNavigationBar: const CustomNavigatorBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
---
+++ qr_reader/lib/widgets/custom_navigatorbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomNavigatorBar extends StatelessWidget {
+   
+  const CustomNavigatorBar({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      elevation: 0,
+      items: const <BottomNavigationBarItem> [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Mapa'
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.compass_calibration),
+          label: 'Direcciones'
+        )
+      ]
+    );
+  }
+}
