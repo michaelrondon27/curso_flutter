a9367418fac3909acd712c6d1fad10b88f94a1a3
Michael Rondon <mrondon72@gmail.com>
2023-01-30 12:17:21 +0100

    288. Implementando el BottomNavigationBar

--- newsapp/lib/src/screens/tabs_screen.dart
+++ newsapp/lib/src/screens/tabs_screen.dart
@@ -6,13 +6,48 @@ class TabsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: PageView(
-        children: [
-          Container(color: Colors.red),
-          Container(color: Colors.green)
-        ]
-      )
+    return const Scaffold(
+      body: _Pages(),
+      bottomNavigationBar: _Navigation(),
+    );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  
+  const _Navigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const <BottomNavigationBarItem> [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Para Ti'
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Encabezados'
+        )
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+
+  const _Pages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.green)
+      ]
     );
   }
 }
