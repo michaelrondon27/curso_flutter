6b3d4134174d13d7ce25e46a52ea97c5752ec1fa
Michael Rondon <mrondon72@gmail.com>
2023-01-30 11:39:54 +0100

    253. ELiminar la transicion entre pantallas

--- productos_app/lib/screens/check_auth_screen.dart
+++ productos_app/lib/screens/check_auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:productos_app/screens/screens.dart';
 import 'package:productos_app/services/services.dart';
 
 class CheckAuthScreen extends StatelessWidget {
@@ -16,13 +17,25 @@ class CheckAuthScreen extends StatelessWidget {
       body: Center(
         child: FutureBuilder(
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (!snapshot.hasData) return const Text('Espere');
+            if (!snapshot.hasData) return const Text('');
 
             if (snapshot.data == '') {
               Future.microtask(() {
-                Navigator.of(context).pushReplacementNamed('login');
+                Navigator.pushReplacement(context, PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const LoginScreen(),
+                  transitionDuration: const Duration(seconds: 0)
+                ));
+              });
+            } else {
+              Future.microtask(() {
+                Navigator.pushReplacement(context, PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const HomeScreen(),
+                  transitionDuration: const Duration(seconds: 0)
+                ));
               });
             }
+
+            return Container();
           },
           future: authService.readToken()
         )
