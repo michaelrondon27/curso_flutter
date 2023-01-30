5744de613236ff29f5f8ba7431109d42e92b20d9
Michael Rondon <mrondon72@gmail.com>
2023-01-30 14:23:01 +0100

    295. Diseño del widget de las noticias

--- newsapp/lib/src/widgets/list_news.dart
+++ newsapp/lib/src/widgets/list_news.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:newsapp/src/models/news_models.dart';
+import 'package:newsapp/src/theme/theme.dart';
 
 class ListNews extends StatelessWidget {
 
@@ -15,9 +16,99 @@ class ListNews extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int i) {
-        return Text(news[i].title);
+        return _News(index: i, news: news[i]);
       },
       itemCount: news.length,
     );
   }
+}
+
+class _News extends StatelessWidget {
+
+  final int index;
+  final Article news;
+
+  const _News({
+    Key? key,
+    required this.index,
+    required this.news
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget> [
+        _CardTopBar(index: index, news: news),
+
+        _CardTitle(news: news),
+
+        _CardImage(news: news)
+      ]
+    );
+  }
+
+}
+
+class _CardTopBar extends StatelessWidget {
+
+  final int index;
+  final Article news;
+  
+  const _CardTopBar({
+    Key? key,
+    required this.index,
+    required this.news
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: <Widget> [
+          Text('${index + 1}. ', style: TextStyle(color: myTheme.colorScheme.secondary)),
+          Text('${news.source.name}. ')
+        ]
+      )
+    );
+  }
+
+}
+
+class _CardTitle extends StatelessWidget {
+
+  final Article news;
+  
+  const _CardTitle({
+    Key? key,
+    required this.news
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(news.title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700))
+    );
+  }
+
+}
+
+class _CardImage extends StatelessWidget {
+
+  final Article news;
+  
+  const _CardImage({
+    Key? key,
+    required this.news
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+
 }
