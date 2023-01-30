a9fe864b6cf165c232397c21138e3fbfc0a0480f
Michael Rondon <mrondon72@gmail.com>
2023-01-30 15:05:56 +0100

    300. Puliendo el menu de categorias

--- newsapp/lib/src/screens/tab2_screen.dart
+++ newsapp/lib/src/screens/tab2_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:provider/provider.dart';
 
 import 'package:newsapp/src/services/news_service.dart';
@@ -31,18 +32,51 @@ class _CategoryList extends StatelessWidget {
     final categories = Provider.of<NewsService>(context).categories;
 
     return ListView.builder(
-      itemBuilder: (BuildContext context, int i) => Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Icon(categories[i].icon),
-            const SizedBox(height: 5),
-            Text(categories[i].name)
-          ]
-        )
-      ),
+      itemBuilder: (BuildContext context, int i) {
+        
+        final cName = categories[i].name;
+
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              _CategoryButton(category: categories[i]),
+              const SizedBox(height: 5),
+              Text('${cName[0].toUpperCase()}${cName.substring(1)}')
+            ]
+          )
+        );
+      },
       itemCount: categories.length,
       scrollDirection: Axis.horizontal
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  
+  final CategoryModel category;
+
+  const _CategoryButton({
+    Key? key,
+    required this.category
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        height: 40,
+        width: 40,
+        child: Icon(category.icon, color: Colors.black54)
+      ),
     );
   }
 }
